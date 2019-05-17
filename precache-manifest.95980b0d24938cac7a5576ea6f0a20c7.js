6a59b0d51b3f9f41f0f0a9e89ba0c585bd3edd9b
Anwar Abdullah <anwarabdullahn28@gmail.com>
2019-05-18 00:37:18 +0700

    Updates

---
+++ precache-manifest.95980b0d24938cac7a5576ea6f0a20c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd5f7be4feff3e18376fe1a03697740",
+    "url": "/try-true-winner/index.html"
+  },
+  {
+    "revision": "89aaaab2c4f72a79835a",
+    "url": "/try-true-winner/static/css/2.1ad4406c.chunk.css"
+  },
+  {
+    "revision": "eeece543cc439057a407",
+    "url": "/try-true-winner/static/css/main.76af46b9.chunk.css"
+  },
+  {
+    "revision": "89aaaab2c4f72a79835a",
+    "url": "/try-true-winner/static/js/2.ee0a941e.chunk.js"
+  },
+  {
+    "revision": "eeece543cc439057a407",
+    "url": "/try-true-winner/static/js/main.707a8f23.chunk.js"
+  },
+  {
+    "revision": "31133cbfda0c35bb22aa",
+    "url": "/try-true-winner/static/js/runtime~main.a5aed8d3.js"
+  }
+]);
